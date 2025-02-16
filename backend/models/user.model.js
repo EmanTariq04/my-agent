1914fc7288f2dc88b3aafa43f47d7dda10cb1351
Eman Tariq <emantariq.1004@gmail.com>
2025-02-16 21:24:13 +0500

    initial commit

---
+++ backend/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: [6, "Email must be atleast 6 characters long"],
+    maxlength: [50, "Email must not be longer than 50 characters"],
+  },
+
+  password: {
+    type: String,
+    select: false,
+  },
+});
+
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+userSchema.methods.isValidPssword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateJWT = function () {
+  return jwt.sign({ email: this.email }, process.env.JWT_SECRET);
+};
+
+const user = mongoose.model("user", userSchema);
+
+export default user;
