aa2f372edb812b1a66a7e6810920ae2aa7d0981e
Eman Tariq <emantariq.1004@gmail.com>
2025-02-19 22:09:57 +0500

    creating project ui

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -28,7 +28,16 @@ const Home = () => {
       });
   }
 
-
+  useEffect(() => {
+    axios
+      .get("/projects/all")
+      .then((res) => {
+        setProject(res.data.projects);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
   return (
     <main className="p-4">
